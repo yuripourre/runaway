ff63d44820df25c9da11fb53846b1e66a5a69116
yuripourre <yuriamayuri@gmail.com>
2014-08-31 13:04:30 -0400

    Rotating player

--- src/br/com/runaway/GameApplication.java
+++ src/br/com/runaway/GameApplication.java
@@ -7,22 +7,39 @@ import br.com.etyllica.core.event.GUIEvent;
 import br.com.etyllica.core.event.KeyEvent;
 import br.com.etyllica.core.event.PointerEvent;
 import br.com.etyllica.core.graphics.Graphic;
+import br.com.etyllica.layer.AnimatedLayer;
 import br.com.etyllica.layer.ImageLayer;
 
 public class GameApplication extends Application {
 
-	private ImageLayer player;
+	private AnimatedLayer player;
 	
 	public GameApplication(int w, int h) {
 		super(w, h);
 	}
-
+	
+	private int turnSpeed = 5;
+	private boolean rightArrow = false;
+	private boolean leftArrow = false;
+	
+	
 	@Override
 	public void load() {
 		
-		player = new ImageLayer(0, 0, 96,64, "player/player_walk_strip6.png");
+		player = new AnimatedLayer(0, 60, 32, 48, "player/player_walk_strip.png");
+		
+		updateAtFixedRate(30);
 		
 		loading = 100;
+	}
+	
+	public void timeUpdate(long now) {
+		if(rightArrow) {
+			player.setOffsetAngle(turnSpeed);
+		}
+		if(leftArrow) {
+			player.setOffsetAngle(-turnSpeed);
+		}
 	}
 
 	@Override
@@ -41,6 +58,19 @@ public class GameApplication extends Application {
 
 	@Override
 	public GUIEvent updateKeyboard(KeyEvent event) {
+		
+		if(event.isKeyDown(KeyEvent.TSK_RIGHT_ARROW)) {
+			rightArrow = true;
+		} else if(event.isKeyUp(KeyEvent.TSK_RIGHT_ARROW)) {
+			rightArrow = false;
+		}
+		
+		if(event.isKeyDown(KeyEvent.TSK_LEFT_ARROW)) {
+			leftArrow = true;
+		}else if(event.isKeyUp(KeyEvent.TSK_LEFT_ARROW)) {
+			leftArrow = false;
+		}
+		
 		// TODO Auto-generated method stub
 		return null;
 	}
