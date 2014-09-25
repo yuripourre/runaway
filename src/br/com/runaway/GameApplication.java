5ad11a8d4152fe3ebb8a083741733c84ce3db5fd
Yuri Pourre <yuriamayuri@gmail.com>
2014-09-25 03:52:38 -0300

    Draw two cameras

--- src/br/com/runaway/GameApplication.java
+++ src/br/com/runaway/GameApplication.java
@@ -1,8 +1,10 @@
 package br.com.runaway;
 
 import java.awt.Color;
+import java.awt.geom.AffineTransform;
 import java.io.FileNotFoundException;
 
+import br.com.etyllica.cinematics.Camera;
 import br.com.etyllica.collision.CollisionDetector;
 import br.com.etyllica.context.Application;
 import br.com.etyllica.core.event.GUIEvent;
@@ -23,95 +25,119 @@ import br.com.vite.editor.MapEditor;
 import br.com.vite.export.MapExporter;
 
 public class GameApplication extends Application {
-	
+
+	private Camera camera1;
+	private Camera camera2;
+
 	private MapEditor map;
-	
+
 	private TopViewPlayer player;
-	
+
 	private TopViewPlayer player2;
-	
+
 	private Controller firstPlayerController;
-	
+
 	private Controller secondPlayerController;
-	
+
 	private Layer obstacle;
-	
+
 	private ShadowLayer shadowMap;
-	
+
 	private LightSource torch1;
-	
+
 	private LightSource torch2;
-	
+
 	private SpikeFloor trap;
-	
+
 	public GameApplication(int w, int h) {
 		super(w, h);
 	}
-	
+
 	@Override
 	public void load() {
+
+		camera1 = new Camera(0, 0, w/2, h);
+		camera2 = new Camera(w/2, 0, w/2, h);
 		
 		player = new TopViewPlayer(w/4, h/2);
-		
-		player2 = new TopViewPlayer(w/2+w/4, h/2);
-		
+
+		player2 = new TopViewPlayer(w/4+20, h/2+80);
+
 		firstPlayerController = new FirstPlayerController(player);
-		
+
 		secondPlayerController = new EasyController(player2);
-		
+
 		updateAtFixedRate(30);
-		
+
 		obstacle = new Layer(200,200,100,50);
 		obstacle.setAngle(20);
-		
+
 		shadowMap = new ShadowLayer(x, y, w, h);
 		torch1 = new LightSource(player.getX(), player.getY(), 120);
 		torch2 = new LightSource(player2.getX(), player2.getY(), 120);
-	
+
 		try {
 			map = MapExporter.load("map1.json");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		trap = new SpikeFloor(w/2, h/2);
-		
+
 		loading = 100;
 	}
-	
+
 	public void timeUpdate(long now) {
 		player.update(now);
 		player2.update(now);
-		
+
 		trap.update(now);
-		
+
 		int p1x = player.getX()+player.getLayer().getTileW()/2;
 		int p1y = player.getY()+player.getLayer().getTileH()/2;
-		
+
 		torch1.setCoordinates(p1x-torch1.getW()/2, p1y-torch1.getH()/2);
-		
+
 		int p2x = player2.getX()+player2.getLayer().getTileW()/2;
 		int p2y = player2.getY()+player2.getLayer().getTileH()/2;
-		
+
 		torch2.setCoordinates(p2x-torch2.getW()/2, p2y-torch2.getH()/2);
+		
+		//camera1.setAimLocation(player.getX(), 0);
+		//camera2.setAimLocation(player2.getX(), 0);
 	}
 
 	@Override
 	public void draw(Graphic g) {
-		
+
+		//Draw First Player Camera
+		g.setCamera(camera1);
+		drawScene(g);
+		g.resetCamera(camera1);
+		camera1.draw(g);
+
+		//Draw Second Player Camera
+		g.setCamera(camera2);
+		drawScene(g);
+		g.resetCamera(camera2);
+		camera2.draw(g);
+
+	}
+
+	private void drawScene(Graphic g) {
 		map.draw(g);
-		
+
 		trap.draw(g);
-		
+
 		/*g.setColor(Color.GREEN);
 		g.fillRect(0, 0, w/2, h);
 		g.setColor(Color.BLUE);
 		g.fillRect(w/2, 0, w/2, h);*/
-		
+
 		player.draw(g);
 		player2.draw(g);
-		
+
 		g.setColor(Color.BLACK);
 		for(Point2D point: CollisionDetector.getBounds(player.getHitbox())) {
 			g.fillCircle(point, 5);
@@ -119,18 +145,18 @@ public class GameApplication extends Application {
 		for(Point2D point: CollisionDetector.getBounds(obstacle)) {
 			g.fillCircle(point, 5);
 		}
-		
+
 		if(CollisionDetector.colidePolygon(player.getHitbox(), obstacle)) {
 			g.setColor(SVGColor.SAVAGE_BLUE);
 		} else {
 			g.setColor(SVGColor.CORN_SILK);
 		}
-				
+
 		g.fillRect(obstacle);
-		
+
 		//shadowMap.drawLights(g, torch1, torch2);
 	}
-	
+
 	@Override
 	public GUIEvent updateMouse(PointerEvent event) {
 		// TODO Auto-generated method stub
@@ -139,10 +165,10 @@ public class GameApplication extends Application {
 
 	@Override
 	public GUIEvent updateKeyboard(KeyEvent event) {
-		
+
 		firstPlayerController.handleEvent(event);
 		secondPlayerController.handleEvent(event);
-		
+
 		return null;
 	}
 
